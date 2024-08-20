e21e2470d1a7c604f177216f8b4609f9b826463e
sebas <sebdeveloper6952@gmail.com>
2024-08-20 07:24:57 -0600

    add subject model

--- lib/wanikani/models.dart
+++ lib/wanikani/models.dart
@@ -19,6 +19,70 @@ class User {
   });
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
-
   Map<String, dynamic> toJson() => _$UserToJson(this);
+}
+
+@JsonSerializable()
+class AuxiliaryMeaning {
+  final String meaning;
+  final String type;
+
+  AuxiliaryMeaning({
+    required this.meaning,
+    required this.type,
+  });
+
+  factory AuxiliaryMeaning.fromJson(Map<String, dynamic> json) =>
+      _$AuxiliaryMeaningFromJson(json);
+  Map<String, dynamic> toJson() => _$AuxiliaryMeaningToJson(this);
+}
+
+@JsonSerializable()
+class Meaning {
+  final String meaning;
+  final bool primary;
+  final bool acceptedAnswer;
+
+  Meaning({
+    required this.meaning,
+    required this.primary,
+    required this.acceptedAnswer,
+  });
+
+  factory Meaning.fromJson(Map<String, dynamic> json) =>
+      _$MeaningFromJson(json);
+  Map<String, dynamic> toJson() => _$MeaningToJson(this);
+}
+
+@JsonSerializable()
+class Subject {
+  final String characters;
+  final String documentUrl;
+  final int lessonPosition;
+  final int level;
+  final String meaningMnemonic;
+  final String slug;
+  final int spacedRepetitionSystemId;
+  final DateTime createdAt;
+  final DateTime? hiddenAt;
+  final List<AuxiliaryMeaning> auxiliaryMeanings;
+  final List<Meaning> meanings;
+
+  Subject({
+    required this.characters,
+    required this.documentUrl,
+    required this.lessonPosition,
+    required this.level,
+    required this.meaningMnemonic,
+    required this.slug,
+    required this.spacedRepetitionSystemId,
+    required this.createdAt,
+    required this.hiddenAt,
+    required this.auxiliaryMeanings,
+    required this.meanings,
+  });
+
+  factory Subject.fromJson(Map<String, dynamic> json) =>
+      _$SubjectFromJson(json);
+  Map<String, dynamic> toJson() => _$SubjectToJson(this);
 }
--- lib/wanikani/models.g.dart
+++ lib/wanikani/models.g.dart
@@ -35,3 +35,101 @@ Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'started_at': instance.startedAt.toIso8601String(),
       'username': instance.username,
     };
+
+AuxiliaryMeaning _$AuxiliaryMeaningFromJson(Map<String, dynamic> json) =>
+    $checkedCreate(
+      'AuxiliaryMeaning',
+      json,
+      ($checkedConvert) {
+        final val = AuxiliaryMeaning(
+          meaning: $checkedConvert('meaning', (v) => v as String),
+          type: $checkedConvert('type', (v) => v as String),
+        );
+        return val;
+      },
+    );
+
+Map<String, dynamic> _$AuxiliaryMeaningToJson(AuxiliaryMeaning instance) =>
+    <String, dynamic>{
+      'meaning': instance.meaning,
+      'type': instance.type,
+    };
+
+Meaning _$MeaningFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'Meaning',
+      json,
+      ($checkedConvert) {
+        final val = Meaning(
+          meaning: $checkedConvert('meaning', (v) => v as String),
+          primary: $checkedConvert('primary', (v) => v as bool),
+          acceptedAnswer: $checkedConvert('accepted_answer', (v) => v as bool),
+        );
+        return val;
+      },
+      fieldKeyMap: const {'acceptedAnswer': 'accepted_answer'},
+    );
+
+Map<String, dynamic> _$MeaningToJson(Meaning instance) => <String, dynamic>{
+      'meaning': instance.meaning,
+      'primary': instance.primary,
+      'accepted_answer': instance.acceptedAnswer,
+    };
+
+Subject _$SubjectFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'Subject',
+      json,
+      ($checkedConvert) {
+        final val = Subject(
+          characters: $checkedConvert('characters', (v) => v as String),
+          documentUrl: $checkedConvert('document_url', (v) => v as String),
+          lessonPosition:
+              $checkedConvert('lesson_position', (v) => (v as num).toInt()),
+          level: $checkedConvert('level', (v) => (v as num).toInt()),
+          meaningMnemonic:
+              $checkedConvert('meaning_mnemonic', (v) => v as String),
+          slug: $checkedConvert('slug', (v) => v as String),
+          spacedRepetitionSystemId: $checkedConvert(
+              'spaced_repetition_system_id', (v) => (v as num).toInt()),
+          createdAt:
+              $checkedConvert('created_at', (v) => DateTime.parse(v as String)),
+          hiddenAt: $checkedConvert('hidden_at',
+              (v) => v == null ? null : DateTime.parse(v as String)),
+          auxiliaryMeanings: $checkedConvert(
+              'auxiliary_meanings',
+              (v) => (v as List<dynamic>)
+                  .map((e) =>
+                      AuxiliaryMeaning.fromJson(e as Map<String, dynamic>))
+                  .toList()),
+          meanings: $checkedConvert(
+              'meanings',
+              (v) => (v as List<dynamic>)
+                  .map((e) => Meaning.fromJson(e as Map<String, dynamic>))
+                  .toList()),
+        );
+        return val;
+      },
+      fieldKeyMap: const {
+        'documentUrl': 'document_url',
+        'lessonPosition': 'lesson_position',
+        'meaningMnemonic': 'meaning_mnemonic',
+        'spacedRepetitionSystemId': 'spaced_repetition_system_id',
+        'createdAt': 'created_at',
+        'hiddenAt': 'hidden_at',
+        'auxiliaryMeanings': 'auxiliary_meanings'
+      },
+    );
+
+Map<String, dynamic> _$SubjectToJson(Subject instance) => <String, dynamic>{
+      'characters': instance.characters,
+      'document_url': instance.documentUrl,
+      'lesson_position': instance.lessonPosition,
+      'level': instance.level,
+      'meaning_mnemonic': instance.meaningMnemonic,
+      'slug': instance.slug,
+      'spaced_repetition_system_id': instance.spacedRepetitionSystemId,
+      'created_at': instance.createdAt.toIso8601String(),
+      'hidden_at': instance.hiddenAt?.toIso8601String(),
+      'auxiliary_meanings':
+          instance.auxiliaryMeanings.map((e) => e.toJson()).toList(),
+      'meanings': instance.meanings.map((e) => e.toJson()).toList(),
+    };
